8048111071457a639b9310d99d6d08af554d803c
Matthias Mailänder <Mailaender@users.noreply.github.com>
2021-12-11 21:14:01 +0100

    Update to current release.

--- openra/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$id     = "openra"
-$url    = "https://github.com/OpenRA/OpenRA/releases/download/release-20141029/OpenRA-release-20141029.exe"
-$kind   = "EXE"
-$silent = "/S"
-
-Install-ChocolateyPackage -PackageName $id -FileType $kind -SilentArgs $silent -Url $url
---
+++ release/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'openra'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://github.com/OpenRA/OpenRA/releases/download/release-20210321/OpenRA-release-20210321-x64.exe'
+$checksum = '77accf828ac7f02e87c4f0e32cfa95d1'
+$checksumType = 'md5'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
